1bde36c8d00f7aaf9fe43aec2c350167c2c4890d
djnatedagreat <nfrank@nysenate.gov>
2026-07-14 13:44:52 -0400

    moved local storage to ~/.bluebird-utils

--- config/filesystems.php
+++ config/filesystems.php
@@ -1,10 +1,16 @@
 <?php
+$home = getenv('HOME') ?: getenv('USERPROFILE');
+$root = rtrim($home, '/\\') . DIRECTORY_SEPARATOR . '.bluebird-utils';
+if (!is_dir($root)) {
+  mkdir($root, 0755, true);
+}
+
 return [
   'default' => 'local',
   'disks' => [
     'local' => [
       'driver' => 'local',
-      'root' => getcwd(),
+      'root' => $root,
     ],
   ],
 ];
